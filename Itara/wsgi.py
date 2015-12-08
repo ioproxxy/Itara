af13e3417be5e9cf4069a399835b30122c6aac04
Ishmael Nyunya <ioproxxy@gmail.com>
2015-12-08 13:42:17 +0300

    Change the wsgi.py

--- Itara/wsgi.py
+++ Itara/wsgi.py
@@ -9,12 +9,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Itara.settings")
-
-#application = get_wsgi_application()
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
