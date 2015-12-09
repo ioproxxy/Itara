987f779a4b77b66e2740b7fb0dae36dafd711d17
Ishmael Nyunya <ioproxxy@gmail.com>
2015-12-09 17:40:37 +0300

    Added properties model

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Property(models.Model):
+	Price = models.IntegerField(max_length=50)
+	ContractType = models.CharField(max_length=50)
+	Type = models.CharField(max_length=50)
+	LocationName= models.CharField(max_length=50)
+	Latitude= models.FloatField(max_length=50)
+	Longitude= models.FloatField(max_length=50)
+	Area= models.CharField(max_length=50)
+	FeaturesDesc= models.TextField()
+	
